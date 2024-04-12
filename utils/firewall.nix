f56b0109695d03ec82661f00a2beafe533709fe6
mdnlss <rob73hall@gmail.com>
2024-04-12 15:12:58 -0400

    feat: update for proper local send ports

--- utils/firewall.nix
+++ utils/firewall.nix
@@ -3,6 +3,7 @@
   networking.firewall = {
    enable = true;
    package = pkgs.nftables;
-   allowedTCPPorts = [ 80 443 22 ] ++ (if enable_localsend then [55137] else []);
+   allowedTCPPorts = [ 80 443 22 ] ++ (if enable_localsend then [53317] else []);
+   allowedUDPPorts = [] ++ (if enable_localsend then [53317] else []);
   };
 }
